89ba2357389d17b66f1208e5ce8b0aadf5baf969
community-vercel <community-vercel>
2025-06-23 10:10:31 -0700

    aaa

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
