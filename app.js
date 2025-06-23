5f190ed2d38f10a0268302b77e0f368322d72c27
community-vercel <community-vercel>
2025-06-23 10:39:56 -0700

    aaa

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
