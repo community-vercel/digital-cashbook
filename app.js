22b61c748400412e772255cf0f3f5bdfffadb31a
community-vercel <community-vercel>
2025-06-23 10:39:38 -0700

    aa

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(cors());
-app.options('*', cors()); // Preflight
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 
 app.use(express.json());
