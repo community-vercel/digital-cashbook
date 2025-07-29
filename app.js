93a4adbafa2747800b0de86d4cdc26290ccbd581
community-vercel <communityvercel@gmail.com>
2025-07-30 01:15:43 +0500

    aaa

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ const backupRoutes = require('./routes/backup');
 require('dotenv').config();
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://client-phi-orcin.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://client-phi-orcin.vercel.app',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+//   })
+// );
 // Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
